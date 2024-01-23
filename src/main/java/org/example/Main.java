cf128305862b652c840e1be93525bde35406b838
Jason Fulghum <jason@dolthub.com>
2024-01-23 14:28:32 -0800

    Running the table creation test statement 100 times in a loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,8 +8,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:11229/jan23", "root", "");
         Statement stmt = connection.createStatement();
-        stmt.executeUpdate("DROP TABLE IF EXISTS formstatusflow;");
-        stmt.executeUpdate("CREATE TABLE `formstatusflow` ( `StatusflowID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT , `SagID` int(11) UNSIGNED NOT NULL , `FromStatusID` int(11) UNSIGNED NOT NULL , `ToStatusID` int(11) UNSIGNED NOT NULL , `UUID` char(36) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL , `InSync` tinyint(1) NOT NULL DEFAULT 1 , `Timestamp` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP , PRIMARY KEY (`StatusflowID`), FOREIGN KEY (`FromStatusID`) REFERENCES `formstatus` (`StatusID`) ON DELETE CASCADE ON UPDATE CASCADE, FOREIGN KEY (`ToStatusID`) REFERENCES `formstatus` (`StatusID`) ON DELETE CASCADE ON UPDATE CASCADE, INDEX `Statusflow.ToStatusID` (`ToStatusID`) USING BTREE , INDEX `StatusFlow.FromStatusID` (`FromStatusID`) USING BTREE ) ENGINE=InnoDB DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci AUTO_INCREMENT=1");
+        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS `formstatus` (`StatusID`  int(11) UNSIGNED NOT NULL AUTO_INCREMENT, PRIMARY KEY (`StatusID`) )" +
+                "ENGINE=InnoDB DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci AUTO_INCREMENT=1;");
+
+        for (int i = 0; i < 100; i++) {
+            stmt.executeUpdate("DROP TABLE IF EXISTS formstatusflow;");
+            stmt.executeUpdate("CREATE TABLE `formstatusflow` ( `StatusflowID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT , `SagID` int(11) UNSIGNED NOT NULL , `FromStatusID` int(11) UNSIGNED NOT NULL , `ToStatusID` int(11) UNSIGNED NOT NULL , `UUID` char(36) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL , `InSync` tinyint(1) NOT NULL DEFAULT 1 , `Timestamp` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP , PRIMARY KEY (`StatusflowID`), FOREIGN KEY (`FromStatusID`) REFERENCES `formstatus` (`StatusID`) ON DELETE CASCADE ON UPDATE CASCADE, FOREIGN KEY (`ToStatusID`) REFERENCES `formstatus` (`StatusID`) ON DELETE CASCADE ON UPDATE CASCADE, INDEX `Statusflow.ToStatusID` (`ToStatusID`) USING BTREE , INDEX `StatusFlow.FromStatusID` (`FromStatusID`) USING BTREE ) ENGINE=InnoDB DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci AUTO_INCREMENT=1");
+        }
         stmt.close();
         connection.close();
     }
